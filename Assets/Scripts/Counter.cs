1bd4c27c1453cdc0b8395a61b449a718eceedd15
writel1ne <70217377+writel1ne@users.noreply.github.com>
2024-04-29 18:14:25 +0300

    .

--- Assets/Scripts/Counter.cs
+++ Assets/Scripts/Counter.cs
@@ -3,47 +3,47 @@ using UnityEngine;
 
 public class Counter : MonoBehaviour
 {
-    [SerializeField] private TMPro.TMP_Text _TextMeshPro;
+	[SerializeField] private TMPro.TMP_Text _TextMeshPro;
 
-    private WaitForSeconds _waitForSeconds = new WaitForSeconds(0.5f);
-    private Coroutine _coroutine;
-    private bool _isRunning = true;
-    private int _time = 0;
+	private WaitForSeconds _waitForSeconds = new WaitForSeconds(0.5f);
+	private Coroutine _coroutine;
+	private bool _isRunning = true;
+	private int _time = 0;
 
-    void Start()
-    {
-        _coroutine = StartCoroutine(UpdateCounter());
-        Debug.Log("Запуск");
-    }
+	void Start()
+	{
+		_coroutine = StartCoroutine(UpdateCounter());
+		Debug.Log("Запуск");
+	}
 
-    void Update()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
-            switch (_isRunning)
-            {
-                case true:
-                    StopCoroutine(_coroutine);
-                    Debug.Log("Остановка");
-                    break;
-                case false:
-                    _coroutine = StartCoroutine(UpdateCounter());
-                    Debug.Log("Запуск");
-                    break;
+	void Update()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			switch (_isRunning)
+			{
+				case true:
+					StopCoroutine(_coroutine);
+					Debug.Log("Остановка");
+					break;
+				case false:
+					_coroutine = StartCoroutine(UpdateCounter());
+					Debug.Log("Запуск");
+					break;
 
-            }
+			}
 
-            _isRunning = !_isRunning;
-        }  
-    }
+			_isRunning = !_isRunning;
+		}
+	}
 
-    IEnumerator UpdateCounter()
-    {
-        while (true)
-        {
-            _time += 1;
-            _TextMeshPro.text = _time.ToString();
-		    yield return _waitForSeconds;
-        }
+	IEnumerator UpdateCounter()
+	{
+		while (true)
+		{
+			_time += 1;
+			_TextMeshPro.text = _time.ToString();
+			yield return _waitForSeconds;
+		}
 	}
 }
